3f2ad662171331fcaf848361dd37e884ad779568
Yuborama <davidjohan2@hotmail.com>
2023-09-21 03:45:02 -0500

    :art: refactor: remove unused import and update background color

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,9 +1,9 @@
 import { AppRegistry } from "react-native";
 import "react-native-gesture-handler";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 // import "react-native-url-polyfill/auto";
-import Navigation from "./navigation";
 import { StatusBar } from "expo-status-bar";
+import Navigation from "./navigation";
 
 export default function App() {
   return (
@@ -13,7 +13,6 @@ export default function App() {
           flex: 1,
           justifyContent: "flex-start",
           alignItems: "flex-start",
-          backgroundColor: "red",
         }}
       >
         <StatusBar style="auto" />
